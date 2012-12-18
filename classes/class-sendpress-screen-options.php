5ab763e91c5e7afe89e11f6716861bad785fd213
Josh Lyford <joshlyford@gmail.com>
2012-12-18 16:40:24 -0500

    Screen Options Started

---
+++ classes/class-sendpress-screen-options.php
@@ -0,0 +1,24 @@
+<?php
+
+// Prevent loading this file directly
+if ( !defined('SENDPRESS_VERSION') ) {
+	header('HTTP/1.0 403 Forbidden');
+	die;
+}
+
+if(!class_exists('SendPress_Screen_Options')){  
+
+class SendPress_Screen_Options {
+
+	function SendPress_Screen_Options(){
+		add_filter('set-screen-option', array(&$this,'set_screen_options'), 10, 3);
+
+	}
+	function set_screen_options($status, $option, $value) {
+ 		if ( 'sendpress_emails_per_page' == $option ) return $value;
+ 		if ( 'sendpress_reports_per_page' == $option ) return $value;
+	}
+
+}
+
+}
