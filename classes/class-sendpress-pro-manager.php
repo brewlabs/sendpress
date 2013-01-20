23d548ea48f602419136d2b83328d3253b2b899c
Jared Harbour <jaredharbour@gmail.com>
2013-01-20 01:01:40 -0500

    added functions to pro manager class to get and set the pro key state added activate and deactivate functions that will return true if we get data from the store, false otherwise changed activate and deactivate functions in view to call new functions and if false is returned displays a message (this should only happen if the store is down. made it so if the weekly check key function is called and no key is in the DB the transient gets set to deactivated and a renew time of 1 year.  if the user enters a key the activate method will re-write the transient back to normal.

--- classes/class-sendpress-pro-manager.php
+++ classes/class-sendpress-pro-manager.php
@@ -34,26 +34,113 @@ class SendPress_Pro_Manager {
 		add_action( 'admin_head', array( $this, 'check_api_key' ) );
 	}
 
+	/**
+     * get_pro_state
+     * 
+     *
+     * @access public
+     *
+     * @return SENDPRESS_PRO_{state} 
+     */
+	function get_pro_state(){
+		if ( false === ( $state = get_transient( 'sendpress_key_state' ) ) ) {
+		    // It wasn't there, so regenerate the data and save the transient
+		    $sendpress_key_state = $this->check_key();
+			$this->set_pro_state($sendpress_key_state);
+			//set_transient( 'sendpress_key_state', $sendpress_key_state['state'], $sendpress_key_state['transient_time'] );
+			$state = $sendpress_key_state['state'];
+		    
+		}
+		return $state;
+	}
+
+	function set_pro_state($data){
+		if( is_array($data) ){
+			set_transient( 'sendpress_key_state', $data['state'], $data['transient_time'] );
+		}else{
+			delete_transient( 'sendpress_key_state' );
+		}
+	}
+
 	function check_api_key(){
 		if( class_exists('SendPress_Option') ){
 			$key = SendPress_Option::get('api_key');
-			if ( false === ( $state = get_transient( 'sendpress_key_state' ) ) ) {
-			    // It wasn't there, so regenerate the data and save the transient
-			    $sendpress_key_state = $this->check_key($key);
-			    set_transient( 'sendpress_key_state', $sendpress_key_state['state'], $sendpress_key_state['transient_time'] );
-			    $state = $sendpress_key_state['state'];
-			}
+			$state = $this->get_pro_state();
 			
 			if( $state !== 'valid' && !empty($key) ){
 				add_action('sendpress_notices', array($this, 'key_notice'));
 				SendPress_Option::set('api_key','');
 			}
 		}
-
 	}
 
-	function check_key($key){
+	function activate_key($key){
+		
+		$api_params = array( 
+            'edd_action'=> 'activate_license', 
+            'license'   => $key, 
+            'item_name' => urlencode( SENDPRESS_PRO_NAME ) // the name of our product in EDD
+        );
 
+        // Call the custom API.
+        $response = wp_remote_get( add_query_arg( $api_params, SENDPRESS_STORE_URL ), array( 'timeout' => 15, 'sslverify' => false ) );
+
+        // make sure the response came back okay
+        if ( is_wp_error( $response ) )
+            return false;
+
+        // decode the license data
+        $license_data = json_decode( wp_remote_retrieve_body( $response ) );
+
+        if($license_data){
+        	SendPress_Option::set('api_key',$key);
+        	SendPress_Pro_Manager::set_pro_state(array('state'=>$license_data->license, 'transient_time'=>SENDPRESS_TRANSIENT_LENGTH));
+        	//SendPress_Option::set('api_key_state', $license_data->license);
+            //set_transient( 'sendpress_key_state', $license_data->license, SENDPRESS_TRANSIENT_LENGTH );
+            return true;
+    	}
+    	return false;
+	}
+
+	function deactivate_key(){
+
+		$key = SendPress_Option::get('api_key');
+        // data to send in our API request
+        $api_params = array( 
+            'edd_action'=> 'deactivate_license', 
+            'license'   => $key, 
+            'item_name' => urlencode( SENDPRESS_PRO_NAME ) // the name of our product in EDD
+        );
+        
+        // Call the custom API.
+        $response = wp_remote_get( add_query_arg( $api_params, SENDPRESS_STORE_URL ), array( 'timeout' => 15, 'sslverify' => false ) );
+
+        // make sure the response came back okay
+        if ( is_wp_error( $response ) )
+            return false;
+
+        // decode the license data
+        $license_data = json_decode( wp_remote_retrieve_body( $response ) );
+
+        // $license_data->license will be either "deactivated" or "failed"
+        if( $license_data->license === SENDPRESS_PRO_DEACTIVATED || $license_data->license === SENDPRESS_PRO_FAILED ){
+            SendPress_Option::set('api_key','');
+            SendPress_Pro_Manager::set_pro_state(false); //this will delete the transient
+            //delete_transient( 'sendpress_key_state' );
+            return true;
+        }
+        return false;
+	}
+
+	function check_key(){
+		$key = SendPress_Option::get('api_key');
+
+		if(!empty($key)){
+			return array('state'=>SENDPRESS_PRO_DEACTIVATED, 'transient_time'=>YEAR_IN_SECONDS);
+		}
+
+		$failed = array('state'=>'valid', 'transient_time'=>DAY_IN_SECONDS);
+        
         // data to send in our API request
         $api_params = array( 
             'edd_action'=> 'check_license', 
@@ -67,13 +154,16 @@ class SendPress_Pro_Manager {
         // make sure the response came back okay
         // if response didn't come back, lets set the transient and try again in a day.
         if ( is_wp_error( $response ) )
-            return array('state'=>'valid', 'transient_time'=>DAY_IN_SECONDS);
+            return $failed;
 
         // decode the license data
         $license_data = json_decode( wp_remote_retrieve_body( $response ) );
 
-        // $license_data->license will be either "deactivated" or "failed"
-        return array('state'=>$license_data->license, 'transient_time'=>SENDPRESS_TRANSIENT_LENGTH);
+        if($license_data){
+     	   // $license_data->license will be either "deactivated" or "failed"
+     	   return array('state'=>$license_data->license, 'transient_time'=>SENDPRESS_TRANSIENT_LENGTH);
+    	}
+    	return $failed;
 	}
 
 	function key_notice(){
@@ -82,6 +172,18 @@ class SendPress_Pro_Manager {
 			_e('Alert','sendpress');
 			echo "</b>&nbsp;-&nbsp;";
 			printf(__('Your API key is either invalid or in use on another site. Need help? Visit <a href="http://sendpress.com/support/">SendPress Support</a>','sendpress'), get_transient( 'sendpress_key_state' ) );
+	    echo '</div>';
+	}
+
+	function deactivate_key_notice(){
+		echo '<div class="alert alert-error">';
+			printf(__('There was a problem deactivating your API key.  Try again in a few minutes or visit <a href="http://sendpress.com/support/">SendPress Support</a>','sendpress') );
+	    echo '</div>';
+	}
+
+	function activate_key_notice(){
+		echo '<div class="alert alert-error">';
+			printf(__('There was a problem activating your API key.  Try again in a few minutes or visit <a href="http://sendpress.com/support/">SendPress Support</a>','sendpress') );
 	    echo '</div>';
 	}
 	
