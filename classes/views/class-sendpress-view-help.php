77e1a13cded3543d64b6607c781007c255e19ede
Josh Lyford <joshlyford@gmail.com>
2013-01-21 17:07:47 -0500

    Better info for support requests on help tab.

--- classes/views/class-sendpress-view-help.php
+++ classes/views/class-sendpress-view-help.php
@@ -41,18 +41,21 @@ class SendPress_View_Help extends SendPress_View{
 	}
 
 	function help_support(){
+	
+		
 ?>
 	<b>Basic Support</b>
 	<p>You can get support for the FREE version of SendPress on the <a href="http://wordpress.org/support/plugin/sendpress" target="_blank">WordPress.org forums</a>.<br>Also check our <a href="http://sendpress.com/support" target="_blank">Knowledge Base</a> for help at <a href="http://sendpress.com/support" target="_blank">http://sendpress.com/support</a></p>
 	<br>
 	<b>Premium Support</b>
 	<p>Premium support is available if you have purchased SendPress Pro from SendPress.com. Premium support can be accessed via our support site: <a href="http://sendpress.com/support" target="_blank">http://sendpress.com/support</a> and requires a SendPress.com account.</p>
-
+	
 <?php
 	}
 
 	function help_debug(){
-		global $wp_version,$wpdb;;
+		global $wp_version,$wpdb;
+		$browser = NEW SendPress_Browser();
 		echo "<b>WordPress Version</b>: ". $wp_version."<br>";
 		echo "<b>SendPress Version</b>: ".SENDPRESS_VERSION ."<br>";
 		echo '<b>PHP Version</b>: ' . phpversion(). '<br>';
@@ -95,13 +98,87 @@ class SendPress_View_Help extends SendPress_View{
 	  	 _e('blocked','sendpress');
 	  } else {
 	  	_e('open','sendpress');
-	  }
-	  echo "<br><br><b>Active Plugins:</b><br>";
-	  $plugins = get_option ( 'active_plugins' );
-    foreach ( $plugins as $plugin ) {
-    	$p = get_plugin_data(WP_PLUGIN_DIR."/". $plugin );
-        echo $p['Name'] ." - ".$p['Version']. "<br>";
-    }
+	  } ?><br><br>
+	  	<b>Support Info:</b>
+	  	<textarea readonly="readonly" class="sendpress-sysinfo" id="system-info-textarea" name="sendpress-sysinfo" title="<?php _e( 'To copy the system info, click below then press Ctrl + C (PC) or Cmd + C (Mac).', 'sendpress' ); ?>">
+### Begin System Info ###
+
+## Please include this information when posting support requests ##
+
+Multi-site:               <?php echo is_multisite() ? 'Yes' . "\n" : 'No' . "\n" ?>
+
+SITE_URL:                 <?php echo site_url() . "\n"; ?>
+HOME_URL:                 <?php echo home_url() . "\n"; ?>
+
+SP Version:              <?php echo SENDPRESS_VERSION . "\n"; ?>
+WordPress Version:        <?php echo get_bloginfo( 'version' ) . "\n"; ?>
+
+<?php echo $browser ; ?>
+
+PHP Version:              <?php echo PHP_VERSION . "\n"; ?>
+MySQL Version:            <?php echo mysql_get_server_info() . "\n"; ?>
+Web Server Info:          <?php echo $_SERVER['SERVER_SOFTWARE'] . "\n"; ?>
+
+PHP Memory Limit:         <?php echo ini_get( 'memory_limit' ) . "\n"; ?>
+PHP Post Max Size:        <?php echo ini_get( 'post_max_size' ) . "\n"; ?>
+
+WP_DEBUG:                 <?php echo defined( 'WP_DEBUG' ) ? WP_DEBUG ? 'Enabled' . "\n" : 'Disabled' . "\n" : 'Not set' . "\n" ?>
+
+WP Table Prefix:          <?php echo "Length: ". strlen( $wpdb->prefix ); echo " Status:"; if ( strlen( $wpdb->prefix )>16 ) {echo " ERROR: Too Long";} else {echo " Acceptable";} echo "\n"; ?>
+
+Show On Front:            <?php echo get_option( 'show_on_front' ) . "\n" ?>
+Page On Front:            <?php $id = get_option( 'page_on_front' ); echo get_the_title( $id ) . ' #' . $id . "\n" ?>
+Page For Posts:           <?php $id = get_option( 'page_on_front' ); echo get_the_title( $id ) . ' #' . $id . "\n" ?>
+
+Session:                  <?php echo isset( $_SESSION ) ? 'Enabled' : 'Disabled'; ?><?php echo "\n"; ?>
+Session Name:             <?php echo esc_html( ini_get( 'session.name' ) ); ?><?php echo "\n"; ?>
+Cookie Path:              <?php echo esc_html( ini_get( 'session.cookie_path' ) ); ?><?php echo "\n"; ?>
+Save Path:                <?php echo esc_html( ini_get( 'session.save_path' ) ); ?><?php echo "\n"; ?>
+Use Cookies:              <?php echo ini_get( 'session.use_cookies' ) ? 'On' : 'Off'; ?><?php echo "\n"; ?>
+Use Only Cookies:         <?php echo ini_get( 'session.use_only_cookies' ) ? 'On' : 'Off'; ?><?php echo "\n"; ?>
+
+UPLOAD_MAX_FILESIZE:      <?php if ( function_exists( 'phpversion' ) ) echo ( edd_let_to_num( ini_get( 'upload_max_filesize' ) )/( 1024*1024 ) )."MB"; ?><?php echo "\n"; ?>
+POST_MAX_SIZE:            <?php if ( function_exists( 'phpversion' ) ) echo ( edd_let_to_num( ini_get( 'post_max_size' ) )/( 1024*1024 ) )."MB"; ?><?php echo "\n"; ?>
+WordPress Memory Limit:   <?php echo ( edd_let_to_num( WP_MEMORY_LIMIT )/( 1024*1024 ) )."MB"; ?><?php echo "\n"; ?>
+WP_DEBUG:                 <?php echo ( WP_DEBUG ) ? __( 'On', 'sendpress' ) : __( 'Off', 'sendpress' ); ?><?php echo "\n"; ?>
+DISPLAY ERRORS:           <?php echo ( ini_get( 'display_errors' ) ) ? 'On (' . ini_get( 'display_errors' ) . ')' : 'N/A'; ?><?php echo "\n"; ?>
+FSOCKOPEN:                <?php echo ( function_exists( 'fsockopen' ) ) ? __( 'Your server supports fsockopen.', 'sendpress' ) : __( 'Your server does not support fsockopen.', 'sendpress' ); ?><?php echo "\n"; ?>
+
+ACTIVE PLUGINS:
+
+<?php
+$plugins = get_plugins();
+$active_plugins = get_option( 'active_plugins', array() );
+
+foreach ( $plugins as $plugin_path => $plugin ):
+	// If the plugin isn't active, don't show it.
+	if ( ! in_array( $plugin_path, $active_plugins ) )
+		continue;
+
+echo $plugin['Name']; ?>: <?php echo $plugin['Version'] ."\n";
+
+endforeach; ?>
+
+CURRENT THEME:
+
+<?php
+if ( get_bloginfo( 'version' ) < '3.4' ) {
+	$theme_data = get_theme_data( get_stylesheet_directory() . '/style.css' );
+	echo $theme_data['Name'] . ': ' . $theme_data['Version'];
+} else {
+	$theme_data = wp_get_theme();
+	echo $theme_data->Name . ': ' . $theme_data->Version;
+}
+?>
+
+
+### End System Info ###
+			</textarea>
+
+
+
+	  <?php
+	 
 
 	}
 
