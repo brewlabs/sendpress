cb8f9a35a12328ee141c2530040caca9bd26f1e1
Josh Lyford <joshlyford@gmail.com>
2013-03-23 18:27:08 -0400

    test

--- sendpress.php
+++ sendpress.php
@@ -6,7 +6,7 @@ Plugin URI: http://sendpress.com
 Description: Easy to manage Email Markteing and Newsletter plugin for WordPress. 
 Author: SendPress
 Author URI: http://sendpress.com/
-DevServer: 0.9.2.1.0
+DevServer: 0.9.2.1.1
 */
 
 if ( !defined('DB_NAME') ) {
