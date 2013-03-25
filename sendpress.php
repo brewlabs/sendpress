719fac1320a0c5aec71ced71c0b5b97e662d59fe
Josh Lyford <joshlyford@gmail.com>
2013-03-25 11:55:09 -0400

    dropit

--- sendpress.php
+++ sendpress.php
@@ -6,7 +6,7 @@ Plugin URI: http://sendpress.com
 Description: Easy to manage Email Markteing and Newsletter plugin for WordPress. 
 Author: SendPress
 Author URI: http://sendpress.com/
-
+testing
 */
 
 if ( !defined('DB_NAME') ) {
