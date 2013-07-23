b3ecfc66b71ea4ab926ee5dfddfd4b710f37d521
Jared Harbour <jaredharbour@gmail.com>
2013-07-22 22:10:19 -0400

    Cleaned up add subscriber page, made it more nice

--- classes/views/class-sendpress-view-subscribers-add.php
+++ classes/views/class-sendpress-view-subscribers-add.php
@@ -19,10 +19,10 @@ class SendPress_View_Subscribers_Add extends SendPress_View_Subscribers {
 		<!-- For plugins, we also need to ensure that the form posts back to our current page -->
 	    <input type="hidden" name="action" value="create-subscriber" />
 	    <input type="hidden" name="listID" value="<?php echo $_GET['listID']; ?>" />
-	    <?php _e('Email','sendpress') ?>: <input type="text" name="email" value="" /><br>
-	    <?php _e('Firstname','sendpress'); ?>: <input type="text" name="firstname" value="" /><br>
-	    <?php _e('Lastname','sendpress'); ?>: <input type="text" name="lastname" value="" /><br>
-	    <?php _e('Status','sendpress'); ?>: <select name="status">
+	    <span class="sublabel"><?php _e('Email','sendpress') ?>:</span><input type="text" name="email" value="" class="regular-text sp-text" /><br>
+	    <span class="sublabel"><?php _e('Firstname','sendpress'); ?>:</span><input type="text" name="firstname" value="" class="regular-text sp-text" /><br>
+	    <span class="sublabel"><?php _e('Lastname','sendpress'); ?>:</span><input type="text" name="lastname" value="" class="regular-text sp-text" /><br>
+	    <span class="sublabel"><?php _e('Status','sendpress'); ?>:</span><select name="status">
 	    			<?php 
 	    				$results =  SendPress_Data::get_statuses();
 	    				foreach($results as $status){
@@ -49,22 +49,26 @@ class SendPress_View_Subscribers_Add extends SendPress_View_Subscribers {
 	<h2><?php _e('Add Subscribers','sendpress'); ?></h2>
 <div class="boxer">
 	<div class="boxer-inner">	
-<div style="width: 300px; margin-right: 25%; padding: 15px;" class="rounded box float-right">
-<?php _e('Emails shoud be written in separate lines. A line could also include a name, which is separated from the email by a comma','sendpress'); ?>.<br><br>
-<strong><?php _e('Correct formats','sendpress'); ?>:</strong><br>
-john@gmail.com<br>
-john@gmail.com, John<br>
-john@gmail.com, John, Doe<br>
-</div>
 
-<form id="subscribers-create" method="post">
-		<!-- For plugins, we also need to ensure that the form posts back to our current page -->
-	    <input type="hidden" name="action" value="create-subscribers" />
-	    <input type="hidden" name="listID" value="<?php echo $_GET['listID']; ?>" />
-	   	<textarea name="csv-add" cols='50' rows='25'></textarea>
-	   	<button type="submit" class="btn btn-primary"><?php _e('Submit','sendpress'); ?></button>
-	   	<?php SendPress_Data::nonce_field(); ?>
-</form>
+		<div class="subscribers-create-container">
+
+			<form id="subscribers-create" method="post">
+					<!-- For plugins, we also need to ensure that the form posts back to our current page -->
+				    <input type="hidden" name="action" value="create-subscribers" />
+				    <input type="hidden" name="listID" value="<?php echo $_GET['listID']; ?>" />
+				   	<textarea name="csv-add"></textarea>
+				   	<button type="submit" class="btn btn-primary"><?php _e('Submit','sendpress'); ?></button>
+				   	<?php SendPress_Data::nonce_field(); ?>
+			</form>
+
+			<div style="width: 25%; padding: 15px;" class="rounded box float-right">
+				<?php _e('Emails shoud be written in separate lines. A line could also include a name, which is separated from the email by a comma','sendpress'); ?>.<br><br>
+				<strong><?php _e('Correct formats','sendpress'); ?>:</strong><br>
+				john@gmail.com<br>
+				john@gmail.com, John<br>
+				john@gmail.com, John, Doe<br>
+			</div>
+		</div>
 </div>
 </div>
 <?php
