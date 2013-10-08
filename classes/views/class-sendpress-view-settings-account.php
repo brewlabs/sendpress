d8d56090713e87e55ace2edcc2ba5f13dece1f1c
pepair <fabus78@hotmail.com>
2013-10-08 11:58:39 -0400

    Add localization

--- classes/views/class-sendpress-view-settings-account.php
+++ classes/views/class-sendpress-view-settings-account.php
@@ -62,7 +62,7 @@ class SendPress_View_Settings_Account extends SendPress_View_Settings {
 <div style="float:right;" >
   <a href="" class="btn btn-large" ><i class="icon-remove"></i> <?php _e( 'Cancel', 'sendpress' ); ?></a> <a href="#" id="save-update" class="btn btn-primary btn-large"><i class="icon-white icon-ok"></i> <?php _e( 'Save', 'sendpress' ); ?></a>
 </div>
-  <h2>Sending Account Setup</h2>
+  <h2><?php _e('Sending Account Setup', 'sendpress'); ?></h2>
   <br class="clear">
 
 <form method="post" id="post">
@@ -75,7 +75,7 @@ class SendPress_View_Settings_Account extends SendPress_View_Settings {
       if ( $c >= 1 ) { $class = "margin-left: 4%"; }
       echo "<div style=' float:left; width: 48%; $class' id='$key'>";
       ?>      
-        <p>&nbsp;<input name="sendpress-sender" type="radio"  <?php if ( $method == $key || strpos(strtolower($key) , $method) > 0 ) { ?>checked="checked"<?php } ?> id="website" value="<?php echo $key; ?>" /> Send Emails via
+        <p>&nbsp;<input name="sendpress-sender" type="radio"  <?php if ( $method == $key || strpos(strtolower($key) , $method) > 0 ) { ?>checked="checked"<?php } ?> id="website" value="<?php echo $key; ?>" /> <?php _e('Send Emails via', 'sendpress'); ?>
         <?php
         echo $sender->label();
         echo "</p><div class='well'>";
@@ -107,7 +107,7 @@ class SendPress_View_Settings_Account extends SendPress_View_Settings {
       if ( $method == $key || strpos(strtolower($key) , $method) > 0 ) { $class = "active"; }
       echo "<div class='tab-pane $class' id='$key'>";
 ?>      
-        <p>&nbsp;<input name="sendpress-sender" type="radio"  <?php if ( $method == $key || strpos(strtolower($key) , $method) > 0 ) { ?>checked="checked"<?php } ?> id="website" value="<?php echo $key; ?>" /> Activate
+        <p>&nbsp;<input name="sendpress-sender" type="radio"  <?php if ( $method == $key || strpos(strtolower($key) , $method) > 0 ) { ?>checked="checked"<?php } ?> id="website" value="<?php echo $key; ?>" /> <?php _e('Activate', 'sendpress'); ?>
         <?php
         echo $sender->label();
         echo "</p><div class='well'>";
@@ -120,13 +120,13 @@ class SendPress_View_Settings_Account extends SendPress_View_Settings {
 </div>
 
 
-<p ><i class="icon-ok"></i> = Currently Active</p>
+<p ><i class="icon-ok"></i> = <?php _e('Currently Active', 'sendpress'); ?></p>
 <?php } ?>
 <br class="clear">
-<h3>Advanced Sending Options</h3>
+<h3><?php _e('Advanced Sending Options', 'sendpress'); ?></h3>
 <div class="boxer form-box">
   <div style="float: right; width: 45%;">
-    <h2>Email Sending Limits</h2>
+    <h2><?php _e('Email Sending Limits', 'sendpres'); ?></h2>
     
 <?php
   $emails_per_day = SendPress_Option::get('emails-per-day');
@@ -137,12 +137,14 @@ class SendPress_View_Settings_Account extends SendPress_View_Settings {
 $offset = get_option( 'gmt_offset' ) * 60 * 60; // Time offset in seconds
 $local_timestamp = wp_next_scheduled('sendpress_cron_action') + $offset;
 //print_r(wp_get_schedules());
+
+prinf( __('You have sent <strong>%s</strong> emails so far today.'), $count_today);
 ?>
-You have sent <strong><?php echo $count_today; ?></strong> emails so far today.<br><br>
-<input type="text" size="6" name="emails-per-day" value="<?php echo $emails_per_day; ?>" /> Emails Per Day<br><br>
-<input type="text" size="6" name="emails-per-hour" value="<?php echo $emails_per_hour; ?>" /> Emails Per Hour 
 <br><br>
-<h2>Email Encoding</h2>
+<input type="text" size="6" name="emails-per-day" value="<?php echo $emails_per_day; ?>" /> <?php _e('Emails Per Day', 'sendpress'); ?><br><br>
+<input type="text" size="6" name="emails-per-hour" value="<?php echo $emails_per_hour; ?>" /> <?php _e('Emails Per Hour', 'sendpress'); ?>
+<br><br>
+<h2><?php _e('Email Encoding', 'sendpress'); ?></h2>
 <?php
   $charset = SendPress_Option::get('email-charset','UTF-8');
  ?>Charset: 
@@ -160,9 +162,9 @@ $charsete = SendPress_Data::get_charset_types();
   }
 ?>
 </select><br>
-Squares or weird characters displaying in your emails select the charset for your language.
+<?php _e('Squares or weird characters displaying in your emails select the charset for your language.', 'sendpress'); ?>
 <br><br>
-Encoding: <select name="email-encoding" id="">
+<?php _e('Encoding:', 'sendpress'); ?> <select name="email-encoding" id="">
 <?php
  $charset = SendPress_Option::get('email-encoding','8bit');
 $charsete = SendPress_Data::get_encoding_types();
@@ -176,17 +178,17 @@ $charsete = SendPress_Data::get_encoding_types();
   }
 ?>
 </select><br>
-Older versions of SendPress used "quoted-printable"
+<?php _e('Older versions of SendPress used "quoted-printable"', 'sendpress'); ?>
 
   <br class="clear">
   </div>  
   <div style="width: 45%; margin-right: 10%">
     <?php $tl =  SendPress_Option::get('autocron','no'); ?>
     <h2>SendPress Pro Auto Cron</h2>
-    <p>At least once every hour we visit your site, just like a "cron" job.<br>There's no setup involved. Easy and hassle free.</p>
-    <button id="sp-enable-cron" <?php if($tl == 'yes'){ echo "style='display:none;'";} ?> class="btn  btn-success">Enable Pro Auto Cron</button><button id="sp-disable-cron" <?php if($tl == 'no'){ echo "style='display:none;'";} ?> class="btn  btn-danger">Disable Pro Auto Cron</button>
-    <p>Get <a href="http://sendpress.com">SendPress Pro</a> to send even faster with connections every 15 minutes!</p>
-    <p>Pro Auto Cron "Free" does collect some data about your website and usage of SendPress. It will not track any user details, so your security and privacy are safe with us.</p>
+    <p><?php _e('At least once every hour we visit your site, just like a "cron" job.<br>There&rsquo;s no setup involved. Easy and hassle free.', 'sendpress'); ?></p>
+    <button id="sp-enable-cron" <?php if($tl == 'yes'){ echo "style='display:none;'";} ?> class="btn  btn-success"><?php _e('Enable', 'sendpress'); ?>  Pro Auto Cron</button><button id="sp-disable-cron" <?php if($tl == 'no'){ echo "style='display:none;'";} ?> class="btn  btn-danger"><?php _e('Disable', 'sendpress'); ?>  Pro Auto Cron</button>
+    <p><?php _e('Get <a href="http://sendpress.com">SendPress Pro</a> to send even faster with connections every 15 minutes!', 'sendpress'); ?></p>
+    <p><?php _e('Pro Auto Cron "Free" does collect some data about your website and usage of SendPress. It will not track any user details, so your security and privacy are safe with us.', 'sendpress'); ?></p>
 
 
 
@@ -206,7 +208,7 @@ echo date_i18n( get_option('date_format') .' '. get_option('time_format'), $loca
 //wp_nonce_field(  basename(__FILE__) ,'_spnonce' );
 wp_nonce_field( $sp->_nonce_value );
 ?>
-<input type="submit" class="btn btn-primary btn-large" value="Save"/> <a href="" class="btn btn-large"><i class="icon-remove"></i> Cancel</a>
+<input type="submit" class="btn btn-primary btn-large" value="<php _e('Save'); ?>"/> <a href="" class="btn btn-large"><i class="icon-remove"></i> <?php _e('Cancel'); ?></a>
 </form>
 <form method="post" id="post" class="form-horizontal">
 <input type="hidden" name="action" value="send-test-email" />
