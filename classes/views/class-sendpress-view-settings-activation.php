c6d2d0cff07fada88765fa72987de711181f3484
pepair <fabus78@hotmail.com>
2013-10-08 12:53:26 -0400

    Add localization

--- classes/views/class-sendpress-view-settings-activation.php
+++ classes/views/class-sendpress-view-settings-activation.php
@@ -46,14 +46,14 @@ class SendPress_View_Settings_Activation extends SendPress_View_Settings {
 		<br class="clear">
 		<br class="clear">
 <div class="boxer form-box">
-	<h2>Public Page Settings</h2>
-	<br><b>Use theme styles:&nbsp;&nbsp;&nbsp;<input type="radio" value="yes" <?php if(SendPress_Option::get('try-theme')=='yes'){ echo "checked='checked'"; } ?> name="try-theme"> Yes&nbsp;&nbsp;&nbsp;<input type="radio" value="no" <?php if(SendPress_Option::get('try-theme')=='no'){ echo "checked='checked'"; } ?> name="try-theme"> No</b>
-	<br>This will attempt to use the WordPress theme functions <code>get_header</code> and <code>get_footer</code> to build the SendPress default public pages.
-	<br><br><b>View the default pages below to see how they look.</b>
+	<h2><?php _e('Public Page Settings', 'sendpress'); ?></h2>
+	<br><b><?php _e('Use theme styles:', 'sendpress'); ?>&nbsp;&nbsp;&nbsp;<input type="radio" value="yes" <?php if(SendPress_Option::get('try-theme')=='yes'){ echo "checked='checked'"; } ?> name="try-theme"> <?php _e('Yes', 'sendpress'); ?>&nbsp;<input type="radio" value="no" <?php if(SendPress_Option::get('try-theme')=='no'){ echo "checked='checked'"; } ?> name="try-theme"> <?php _e('No', 'sendpress');?></label></b>
+	<br><?php _e('This will attempt to use the WordPress theme functions <code>get_header</code> and <code>get_footer</code> to build the SendPress default public pages.', 'sendpress'); ?>
+	<br><br><b><?php _e('View the default pages below to see how they look.', 'sendpress'); ?></b>
 	<ul class="nomargin">
-		<li><a href="<?php echo site_url(); ?>?sendpress=confirm">Confirmation Page</a></li>
-		<li><a href="<?php echo site_url(); ?>?sendpress=manage">Manage Page</a></li>
-		<li><a href="<?php echo site_url(); ?>?sendpress=post">Post Page</a></li>
+		<li><a href="<?php echo site_url(); ?>?sendpress=confirm"><?php _e('Confirmation Page', 'sendpress'); ?></a></li>
+		<li><a href="<?php echo site_url(); ?>?sendpress=manage"><?php _e('Manage Page', 'sendpress'); ?></a></li>
+		<li><a href="<?php echo site_url(); ?>?sendpress=post"><?php _e('Post Page', 'sendpress'); ?></a></li>
 	</ul>
 
 
@@ -61,12 +61,12 @@ class SendPress_View_Settings_Activation extends SendPress_View_Settings {
 	
 		
 <div class="boxer form-box">
-	<h2>Double Opt-in Email</h2>
+	<h2><?php _e('Double Opt-in Email', 'sendpress'); ?></h2>
 	<div style="float: right; width: 45%;"><br>
-		<b>Subject</b><br>
+		<b><?php _e('Subject', 'sendpress'); ?></b><br>
 		<input type="text" name="subject" class="regular-text sp-text" style="width: 100%;" value="<?php echo  stripcslashes($dpost->post_title); ?>"/>
-		<br><br><b>Body</b><br>
-		<?php if(strpos($dpost->post_content,'*|SP:CONFIRMLINK|*'  )  == false){ echo "<div class='alert alert-error'>Missing <b>*|SP:CONFIRMLINK|*</b> in body content.</div>";} ?>
+		<br><br><b><?php _e('Body', 'sendpress'); ?></b><br>
+		<?php if(strpos($dpost->post_content,'*|SP:CONFIRMLINK|*'  )  == false){ echo "<div class='alert alert-error'>" . __('Missing <b>*|SP:CONFIRMLINK|*</b> in body content.', 'sendpress') . "</div>";} ?>
 		<textarea style="width:100%; padding: 8px;" rows="21" name="body"><?php 
 		remove_filter( 'the_content', 'wpautop' );
 		global $post;
@@ -77,15 +77,15 @@ class SendPress_View_Settings_Activation extends SendPress_View_Settings {
 
 	</div>	
 	<div style="width: 45%; margin-right: 10%">
-		<br><b>Send Double Opt-in Email:&nbsp;&nbsp;&nbsp;<input type="radio" value="yes" <?php if(SendPress_Option::get('send_optin_email')=='yes'){ echo "checked='checked'"; } ?> name="optin"> Yes&nbsp;&nbsp;&nbsp;<input type="radio" value="no" <?php if(SendPress_Option::get('send_optin_email')=='no'){ echo "checked='checked'"; } ?> name="optin"> No</b>
-			<br>Keep the spammers, robots and other riff-raff off your list. <br>Read more about why to use double opt-in on out support site.
+		<br><b><?php _e('Send Double Opt-in Email:', 'sendpress'); ?>&nbsp;&nbsp;&nbsp;<input type="radio" value="yes" <?php if(SendPress_Option::get('send_optin_email')=='yes'){ echo "checked='checked'"; } ?> name="optin"> <?php _e('Yes', 'sendpress'); ?>&nbsp;&nbsp;&nbsp;<input type="radio" value="no" <?php if(SendPress_Option::get('send_optin_email')=='no'){ echo "checked='checked'"; } ?> name="optin"> <?php _e('No', 'sendpress'); ?></b>
+			<br><?php _e('Keep the spammers, robots and other riff-raff off your list. <br>Read more about why to use double opt-in on out support site.', 'sendpress'); ?>
 			<br><br><br>
-			<b>Confirmation Page</b><br>
+			<b><?php _e('Confirmation Page', 'sendpress'); ?></b><br>
 			<div class='well'>
-			Select the page a new subcriber will be redirected to after they click the confirmation link in the Double Opt-in Email.<br><br>
+			<?php _e('Select the page a new subcriber will be redirected to after they click the confirmation link in the Double Opt-in Email.', 'sendpress'); ?><br><br>
 			<?php $ctype = SendPress_Option::get('confirm-page'); ?>
-			<input type="radio" name="confirm-page" value="default" <?php if($ctype=='default'){echo "checked='checked'"; } ?> /> Use Default SendPress Page<br><br>
-			<input type="radio" name="confirm-page" value="custom"  <?php if($ctype=='custom'){echo "checked='checked'";} ?>/> Redirect to <select name="confirm-page-id"> 
+			<input type="radio" name="confirm-page" value="default" <?php if($ctype=='default'){echo "checked='checked'"; } ?> /> <?php _e('Use Default SendPress Page', 'sendpress'); ?><br><br>
+			<input type="radio" name="confirm-page" value="custom"  <?php if($ctype=='custom'){echo "checked='checked'";} ?>/> <?php _e('Redirect to', 'sendpress'); ?> <select name="confirm-page-id"> 
  <option value="">
  	<?php $cpageid = SendPress_Option::get('confirm-page-id');?>
 <?php echo esc_attr( __( 'Select page' ) ); ?></option> 
@@ -105,13 +105,13 @@ class SendPress_View_Settings_Activation extends SendPress_View_Settings {
 
 
 			<br>
-			<b>Quick Tags</b>: work in both subject and body.
+			<?php _e('<b>Quick Tags</b>: work in both subject and body.' ,'sendpress'); ?>
 			<div class='well'>
-				<b>*|SP:CONFIRMLINK|*</b> This is required to be in the body of the email.
+				<b>*|SP:CONFIRMLINK|*</b> - <?php _e('This is required to be in the body of the email.' ,'sendpress'); ?>
 				<hr>
-				*|SITE:TITLE|* - Add Website title to the email.
+				*|SITE:TITLE|* - <?php _e('Add Website title to the email.' ,'sendpress'); ?>
 				<hr>
-				*|EMAIL|* - Add the email the user signed up with.
+				*|EMAIL|* - <?php _e('Add the email the user signed up with.' ,'sendpress'); ?>
 			</div>
 		<!--
 		<p><input type="text" class="regular-text" style="width: 100%;"/></p>
