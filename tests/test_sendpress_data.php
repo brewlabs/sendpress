7c6c235d9d0cf707be9dfe2a50ffa78f3c7f84ef
Josh Lyford <joshlyford@gmail.com>
2013-10-25 13:27:54 -0400

    unit tests

---
+++ tests/test_sendpress_data.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+* Test For SendPress Data
+ */
+class WP_Test_SendPress_Data extends WP_UnitTestCase {
+
+        /**
+         * Run a simple test to ensure that the tests are running
+         */
+        function test_tests() {
+                $this->assertTrue( true );
+        }
+
+        function test_nonce() {
+                $this->assertEquals( 'sendpress-is-awesome',
+                        SendPress_Data::nonce() );
+        }
+}
