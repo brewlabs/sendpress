87e7b85fc125494952c08b415c948fbe8e369420
Josh Lyford <joshlyford@gmail.com>
2013-11-07 19:25:40 -0500

    Deploy to pressweba

--- sendpress.php
+++ sendpress.php
@@ -6,7 +6,7 @@ Plugin URI: http://sendpress.com
 Description: Easy to manage Email Marketing and Newsletter plugin for WordPress. 
 Author: SendPress
 Author URI: http://sendpress.com/
-Push
+Deploy Testing
 */
 
 	if ( !defined('DB_NAME') ) {
