f03589db9bd0e1d4d9ce008aa953f2a8ea8b0437
Josh Lyford <joshlyford@gmail.com>
2014-01-14 19:41:19 -0500

    Reports title fix

--- classes/class-sendpress-posts.php
+++ classes/class-sendpress-posts.php
@@ -39,7 +39,7 @@ class SendPress_Posts{
 		//'post_parent' =>  $post->parent_id,
 		'post_password' => $post->post_password,
 		'post_status' => $post->post_status,
-		'post_title' => (empty($post_title))? $post->post_title: $post_titles, 
+		'post_title' => (empty($post_title))? $post->post_title: $post_title, 
 		'post_type' => (empty($post_type))? $post->post_type: $post_type, 
 		);
 
