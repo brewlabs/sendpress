b0497f46af01a2f25944d18f6839318157a76be6
Josh Lyford <joshlyford@gmail.com>
2014-03-28 22:32:50 -0400

    Pro Manager Update

--- classes/class-sendpress-pro-manager.php
+++ classes/class-sendpress-pro-manager.php
@@ -30,12 +30,46 @@ class SendPress_Pro_Manager {
 	}
 
 	function _init(){
-		
-		add_filter('plugins_api', array( $this, 'plugins_api' ),10,3);
-		add_filter('plugins_api_result', array( $this, 'get_pro_details' ),10,3);
-		if(defined('SENDPRESS_PRO_VERSION')){	
+		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'pre_set_site_transient_update_plugins_filter' ) );
+		add_filter('plugins_api', array( $this, 'get_pro_details' ),10,3);
+		add_filter( 'http_request_args', array( $this, 'http_request_args' ), 10, 2 );
+		//add_filter('plugins_api_result', array( $this, 'get_pro_details' ),10,3);
+		if( defined('SENDPRESS_PRO_VERSION') ){	
 			add_action( 'admin_head', array( $this, 'check_api_key' ) );
 		}
+	}
+
+	function pre_set_site_transient_update_plugins_filter( $_transient_data ) {
+
+
+		if( empty( $_transient_data ) ) return $_transient_data;
+
+		if( defined('SENDPRESS_PRO_VERSION') ){
+			$to_send = array( 'slug' => 'sendpress-pro' );
+
+			$api_response = $this->api_request( 'plugin_latest_version', $to_send );
+
+			if( false !== $api_response && is_object( $api_response ) && isset( $api_response->new_version ) ) {
+				if( version_compare( SENDPRESS_PRO_VERSION, $api_response->new_version, '<' ) )
+					$_transient_data->response[$this->name] = $api_response;
+			}
+		}
+		return $_transient_data;
+	}
+
+	/**
+	 * Disable SSL verification in order to prevent download update failures
+	 *
+	 * @param array $args
+	 * @param string $url
+	 * @return object $array
+	 */
+	function http_request_args( $args, $url ) {
+		// If it is an https request and we are performing a package download, disable ssl verification
+		if( strpos( $url, 'https://' ) !== false && strpos( $url, 'edd_action=package_download' ) ) {
+			$args['sslverify'] = false;
+		}
+		return $args;
 	}
 
 	/**
@@ -225,12 +259,49 @@ class SendPress_Pro_Manager {
 	    echo '</div>';
 	}
 	
-	function plugins_api( $res, $action, $args ){
-		if( $action === 'plugin_information' && isset($args->slug) &&$args->slug === 'sendpress-pro' ){
-			return $args;
-		}
-		return false;
+
+
+	/**
+	 * Calls the API and, if successfull, returns the object delivered by the API.
+	 *
+	 * @uses get_bloginfo()
+	 * @uses wp_remote_post()
+	 * @uses is_wp_error()
+	 *
+	 * @param string $_action The requested action.
+	 * @param array $_data Parameters for the API action.
+	 * @return false||object
+	 */
+	private function api_request( $_action, $_data ) {
+
+		global $wp_version;
+
+		if( $_data['slug'] != 'sendpress-pro' )
+			return;
+		
+		$license = SendPress_Option::get('api_key');
+		$product = SendPress_Option::get('api_product','SendPress Pro');
+				
+		$api_params = array( 
+			'edd_action' 	=> 'get_version', 
+			'license' 		=> $license, 
+			'name' 			=> urlencode( $product ),
+			'slug' 			=> 'sendpress-pro',
+			'author'		=> 'SendPress'
+		);
+		$request = wp_remote_post( SENDPRESS_STORE_URL , array( 'timeout' => 15, 'ssverify' => false, 'body' => $api_params ) );
+		
+		if ( !is_wp_error( $request ) ):
+			$request = json_decode( wp_remote_retrieve_body( $request ) );
+			if( $request )
+				$request->sections = maybe_unserialize( $request->sections );
+			return $request;
+		else:
+			return false;
+		endif;
 	}
+
+	
 
 	function get_pro_details( $res, $action, $args ){
 		
