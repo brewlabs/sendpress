6d8da955989548474c5336a2e1671ea998a72f67
Jared Harbour <jared@brewlabs.us>
2014-08-13 18:33:39 -0400

    fuck my life

---
+++ classes/views/class-sendpress-view-settings-forms.php
@@ -0,0 +1,21 @@
+<?php
+
+// Prevent loading this file directly
+if ( !defined('SENDPRESS_VERSION') ) {
+	header('HTTP/1.0 403 Forbidden');
+	die;
+}
+
+class SendPress_View_Settings_Forms extends SendPress_View_Settings {
+
+	function save($post, $sp){
+		
+	}
+
+	function html($sp) {
+
+		?>testing<?php
+
+	}
+
+}
