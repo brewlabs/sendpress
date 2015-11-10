b31ace449fbfdbe98df649475d8c3c64620e6585
Josh Lyford <joshlyford@gmail.com>
2015-11-09 21:31:36 -0500

    remove unused code

--- classes/public-views/class-sendpress-public-view-open.php
+++ classes/public-views/class-sendpress-public-view-open.php
@@ -17,27 +17,18 @@ class SendPress_Public_View_Open extends SendPress_Public_View{
 		$info = $this->data();
 
 		SPNL()->db->subscribers_tracker->open( $info->report , $info->id );
-		//$link = SendPress_Data::track_open($info->id , $info->report , $ip , $this->_device_type, $this->_device );
-		//header('Content-type: image/gif'); 
-		//include(SENDPRESS_PATH . 'img/clear.gif'); 	
+	
+		//Get the filesize of the image for headers
+		$filesize = filesize( SENDPRESS_PATH . 'img/icon.png' );
 
-		//$graphic_http = 'http://yourwebsite.com/img/blank.gif';
-		//$filesize = filesize( SENDPRESS_PATH . 'img/clear.gif' );
-
-		//Full URI to the image
-			$graphic_http = 'http://yourwebsite.com/blank.gif';
-
-			//Get the filesize of the image for headers
-			$filesize = filesize( SENDPRESS_PATH . 'img/icon.png' );
-
-			//Now actually output the image requested (intentionally disregarding if the database was affected)
-			header( 'Pragma: public' );
-			header( 'Expires: 0' );
-			header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
-			header( 'Cache-Control: public ',false );
-			header('Content-type: image/png'); 
-			header( 'Content-Length: '.$filesize );
-			echo file_get_contents( SENDPRESS_PATH . 'img/icon.png' );
+		//Now actually output the image requested (intentionally disregarding if the database was affected)
+		header( 'Pragma: public' );
+		header( 'Expires: 0' );
+		header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
+		header( 'Cache-Control: public ',false );
+		header('Content-type: image/png'); 
+		header( 'Content-Length: '.$filesize );
+		echo file_get_contents( SENDPRESS_PATH . 'img/icon.png' );
 	}
 
 }
