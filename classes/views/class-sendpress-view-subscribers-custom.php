d824aedad4cd0a3918108dcfc6aeea2beb997104
Bill Ux <chelseaux@Chelseas-MacBook-Pro.local>
2016-06-29 19:38:46 -0400

    Merging custom fields initial changes

--- classes/views/class-sendpress-view-subscribers-custom.php
+++ classes/views/class-sendpress-view-subscribers-custom.php
@@ -20,6 +20,18 @@ class SendPress_View_Subscribers_Custom extends SendPress_View_Subscribers {
 				<?php 
 			}
 			$this->panel_end();
+		
+			$this->panel_start('Upgrade to SendPress Pro');
+			if(defined('SENDPRESS_PRO_VERSION')){
+				?>
+				<p><?php _e('You have SendPress Pro Version','sendpress'); ?> <?php echo SENDPRESS_PRO_VERSION; ?> <?php _e('this version does not support custom fields. You will need to update to the latest version','sendpress'); ?>.</p>
+				<?php 
+			} else {
+				?>
+				<p><?php _e('Custom fields requires','sendpress'); ?> <a href="https://sendpress.com" target="_blank"><?php _e('SendPress Pro','sendpress'); ?></a>. <?php _e('Please upgrade or install Pro to start using this feature','sendpress'); ?>.</p>
+				<?php 
+			}
+			$this->panel_end();
 	}
 
 }
