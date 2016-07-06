c2f6b228f6f5b63200f781529cb724dcd83ad5ab
Josh Lyford <joshlyford@gmail.com>
2016-07-05 21:00:01 -0400

    no message

--- tests/test_sendpress_security.php
+++ tests/test_sendpress_security.php
@@ -6,10 +6,10 @@ class WP_Test_SendPress_Security extends WP_UnitTestCase {
 
 	function test_html(){
 		$sec = new SendPress_Security();
-		$html =  "<p style='color:#000;'>red</p>";
+		$html =  "<p style='color:#00000; font-size:#ededed;'>red</p>";
 		
 		$d = $sec->internal_html( $html );
-		$this->assertEquals( "<p style='color:#000;'>red</p>", $d);
+		$this->assertEquals( "<p style='color:#00000; font-size:#ededed;'>red</p>", $d);
 	}
 
 }
