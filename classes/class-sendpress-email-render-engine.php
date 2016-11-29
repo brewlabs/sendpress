189e680e95cbb71720749c739cf4b4ce266cafd5
Josh Lyford <joshlyford@gmail.com>
2016-11-28 20:57:41 -0800

    style props on p tags

--- classes/class-sendpress-email-render-engine.php
+++ classes/class-sendpress-email-render-engine.php
@@ -39,7 +39,7 @@ class SendPress_Email_Render_Engine {
 				$pTags = $dom->getElementsByTagName('p');
 				foreach ($pTags as $pElement) {
 					$px = $pElement->getAttribute('style');
-					$pElement->setAttribute('style', $px .' margin-top:0;margin-bottom:10px;');
+					$pElement->setAttribute('style', ' margin-top:0; margin-bottom:10px; ' . $px );
 				}
 				$return =  $dom->saveHtml();
 			}
