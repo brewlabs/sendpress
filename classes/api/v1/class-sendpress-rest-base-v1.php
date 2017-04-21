90dd4afdbec7153d5d7214bdb1eeec2550bf3d88
Josh Lyford <Josh.Lyford@blackbaud.com>
2017-04-20 21:37:15 -0400

    update controller

--- classes/api/v1/class-sendpress-rest-base-v1.php
+++ classes/api/v1/class-sendpress-rest-base-v1.php
@@ -12,11 +12,11 @@ WP_REST_Server::ALLMETHODS = ‘GET, POST, PUT, PATCH, DELETE’
 
 abstract class SendPress_REST_Base_v1 extends WP_REST_Controller {
 
-	var $version = 1;
-    var $namespace = 'sendpress/v';
-    var $base = 'api';
+	protected $version = 1;
+    protected $namespace = 'spnl/v';
+    protected $base = 'api';
 
     function get_api_base(){
-    	return $this->namespace . $this->version . '/' . $this->base;
+    	return $this->namespace . $this->version;
     }
 }
