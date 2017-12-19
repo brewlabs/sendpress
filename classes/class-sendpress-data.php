33fb95c7f032a11095bcf1b7aedfc1e04545ba43
Josh Lyford <joshlyford@gmail.com>
2017-12-18 21:40:45 -0500

    remove legacy event tracker

--- classes/class-sendpress-data.php
+++ classes/class-sendpress-data.php
@@ -1420,7 +1420,7 @@ class SendPress_Data extends SendPress_DB_Tables {
 					SendPress_Data::update_subscriber_status($list->listID, $id, 4 , false);
 					//( $sid, $rid, $uid, $ip , $device_type, $device, $type='confirm' )
 					//( $sid, $rid, $lid=null, $uid=null, $ip=null, $device_type=null, $device=null, $type='confirm' )
-					SendPress_Data::add_subscriber_event( $id,$report_id,$list->listID,null ,null, null, null, 'bounce');
+					//SendPress_Data::add_subscriber_event( $id,$report_id,$list->listID,null ,null, null, null, 'bounce');
 				}
 			}
 
